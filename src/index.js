508da218709d208ad334682a215bd0cb48e3225c
Sergey Hursevich <hursik94@gmail.com>
2019-02-18 20:25:05 +0300

    Finally task

--- src/index.js
+++ src/index.js
@@ -4,4 +4,22 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var count = 0, arr = [];
+ 
+  for (var i = 0; i < preferences.length; i++) {
+
+    var spichoneeB = preferences[i] - 1;
+    var spichoneeC = preferences[spichoneeB] - 1;
+    var spichoneeA = preferences[spichoneeC] - 1;
+
+    if (spichoneeB != i && spichoneeC != i && spichoneeA == i && arr.indexOf(i) == -1) {
+      arr.push(spichoneeA);
+      arr.push(spichoneeB);
+      arr.push(spichoneeC);
+      count++;
+    }
+
+  }
+
+  return count;
 };
